4d0412785e9dd6cc20acb781f6623c2a3ab5c2c7
alexandre artus <artusalexandre@yahoo.fr>
2023-12-13 11:57:26 +0100

    sans les commentaires mieux

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 console.log("je suis dans la console")
 
-// let toggle = document.getElementById("togg");
-
 function displayDiv(id) {
   console.log("je suis dans la fonction")
   if (document.getElementById(id).style.display == 'block') {
@@ -11,25 +9,3 @@ function displayDiv(id) {
       document.getElementById(id).style.display = 'block';
   }
 }
-// idée de code pour apparition texte en savoir plus
-
-// let togg1 = document.getElementById("togg1");
-// let togg2 = document.getElementById("togg2");
-// let d1 = document.getElementById("d1");
-// let d2 = document.getElementById("d2");
-// togg1.addEventListener("click", () => {
-//   if(getComputedStyle(d1).display != "none"){
-//     d1.style.display = "none";
-//   } else {
-//     d1.style.display = "block";
-//   }
-// })
-
-// function togg(){
-//   if(getComputedStyle(d2).display != "none"){
-//     d2.style.display = "none";
-//   } else {
-//     d2.style.display = "block";
-//   }
-// };
-// togg2.onclick = togg;
