31efb990020f33bce17fc93979e3d578314684da
alexandre artus <artusalexandre@yahoo.fr>
2023-12-13 16:51:09 +0100

    animation au scroll done

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("je suis dans la console")
-
 function displayDiv(id) {
   console.log("je suis dans la fonction")
   if (document.getElementById(id).style.display == 'block') {
@@ -9,3 +7,29 @@ function displayDiv(id) {
       document.getElementById(id).style.display = 'block';
   }
 }
+
+
+
+const ratio = .5;
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: ratio,
+};
+
+const handleIntersect = function(entries, observer) {
+    entries.forEach(function(entry) {
+      if (entry.intersectionRatio > ratio) {
+        console.log("visible")
+        entry.target.classList.remove('reveal-visible')
+        observer.unobserve(entry.target)
+      }
+    })
+  }
+
+const observer = new IntersectionObserver(handleIntersect, options);
+
+document.querySelectorAll('.left-col, .right-col').forEach(function (element) {
+  observer.observe(element);
+});
