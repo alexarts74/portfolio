3b1ef61a85dfffbd52c85623bfaafc3ad9eedc00
alexandre artus <artusalexandre@yahoo.fr>
2024-01-08 12:04:20 +0100

    version english done

--- script.js
+++ script.js
@@ -33,3 +33,37 @@ const observer = new IntersectionObserver(handleIntersect, options);
 document.querySelectorAll('.left-col, .right-col').forEach(function (element) {
   observer.observe(element);
 });
+
+
+
+
+
+// Récupérer les éléments nécessaires
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+  const langLinks = document.querySelectorAll('.lang-switch a');
+  const frElements = document.querySelectorAll('.fr');
+  const enElements = document.querySelectorAll('.en');
+
+  function switchLanguage(lang) {
+      if (lang === 'en') {
+          frElements.forEach(el => el.style.display = 'none');
+          enElements.forEach(el => el.style.display = 'block');
+          langLinks[0].style.display = 'none'; // Masquer le lien "English"
+          langLinks[1].style.display = 'block'; // Afficher le lien "Français"
+      } else {
+          enElements.forEach(el => el.style.display = 'none');
+          frElements.forEach(el => el.style.display = 'block');
+          langLinks[0].style.display = 'block'; // Afficher le lien "English"
+          langLinks[1].style.display = 'none'; // Masquer le lien "Français"
+      }
+  }
+
+  langLinks.forEach(link => {
+      link.addEventListener('click', function(event) {
+          event.preventDefault();
+          const selectedLang = this.getAttribute('data-lang');
+          switchLanguage(selectedLang);
+      });
+  });
+});
