23a483fd24885df83994ea892bc8d317f769122c
alexandre artus <artusalexandre74@gmail.com>
2024-07-11 23:28:38 +0200

    clean code and comment function for 3D who is not working

--- script.js
+++ script.js
@@ -52,13 +52,13 @@ function switchLanguage(lang) {
     if (lang === 'en') {
         frElements.forEach(el => el.style.display = 'none');
         enElements.forEach(el => el.style.display = 'block');
-        langLinks[0].style.display = 'none'; // Masquer le lien "English"
-        langLinks[1].style.display = 'block'; // Afficher le lien "Français"
+        langLinks[0].style.display = 'none';
+        langLinks[1].style.display = 'block';
     } else {
         enElements.forEach(el => el.style.display = 'none');
         frElements.forEach(el => el.style.display = 'block');
-        langLinks[0].style.display = 'block'; // Afficher le lien "English"
-        langLinks[1].style.display = 'none'; // Masquer le lien "Français"
+        langLinks[0].style.display = 'block';
+        langLinks[1].style.display = 'none';
     }
 }
 
@@ -268,70 +268,71 @@ document.addEventListener("DOMContentLoaded", function() {
 //     document.querySelector('body').style.opacity = 1
 // })
 
+// Try 3D effect
 
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// const canvas = document.getElementById('canvas');
+// const context = canvas.getContext('2d');
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 
-let particlesArray = [];
-const numberOfParticles = 200;
-const colors = ['#1A84D6', '#D62839', '#F5B700', '#00B300', '#E13C6C'];
+// let particlesArray = [];
+// const numberOfParticles = 200;
+// const colors = ['#1A84D6', '#D62839', '#F5B700', '#00B300', '#E13C6C'];
 
-class Particle {
-  constructor(x, y, directionX, directionY, size, color) {
-    this.x = x;
-    this.y = y;
-    this.directionX = directionX;
-    this.directionY = directionY;
-    this.size = size;
-    this.color = color;
-  }
+// class Particle {
+//   constructor(x, y, directionX, directionY, size, color) {
+//     this.x = x;
+//     this.y = y;
+//     this.directionX = directionX;
+//     this.directionY = directionY;
+//     this.size = size;
+//     this.color = color;
+//   }
 
-  draw() {
-    context.beginPath();
-    context.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-    context.fillStyle = this.color;
-    context.fill();
-  }
+//   draw() {
+//     context.beginPath();
+//     context.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+//     context.fillStyle = this.color;
+//     context.fill();
+//   }
 
-  update() {
-    if (this.x + this.size > canvas.width || this.x - this.size < 0) {
-      this.directionX = -this.directionX;
-    }
+//   update() {
+//     if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+//       this.directionX = -this.directionX;
+//     }
 
-    if (this.y + this.size > canvas.height || this.y - this.size < 0) {
-      this.directionY = -this.directionY;
-    }
+//     if (this.y + this.size > canvas.height || this.y - this.size < 0) {
+//       this.directionY = -this.directionY;
+//     }
 
-    this.x += this.directionX;
-    this.y += this.directionY;
-    this.draw();
-  }
-}
+//     this.x += this.directionX;
+//     this.y += this.directionY;
+//     this.draw();
+//   }
+// }
 
-function init() {
-  particlesArray = [];
-  for (let i = 0; i < numberOfParticles; i++) {
-    const size = Math.random() * 5 + 1;
-    const x = Math.random() * (canvas.width - size * 2) + size;
-    const y = Math.random() * (canvas.height - size * 2) + size;
-    const directionX = (Math.random() * 0.4) - 0.2;
-    const directionY = (Math.random() * 0.4) - 0.2;
-    const color = colors[Math.floor(Math.random() * colors.length)];
+// function init() {
+//   particlesArray = [];
+//   for (let i = 0; i < numberOfParticles; i++) {
+//     const size = Math.random() * 5 + 1;
+//     const x = Math.random() * (canvas.width - size * 2) + size;
+//     const y = Math.random() * (canvas.height - size * 2) + size;
+//     const directionX = (Math.random() * 0.4) - 0.2;
+//     const directionY = (Math.random() * 0.4) - 0.2;
+//     const color = colors[Math.floor(Math.random() * colors.length)];
 
-    particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
-  }
-}
+//     particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
+//   }
+// }
 
-function animate() {
-  requestAnimationFrame(animate);
-  context.clearRect(0, 0, canvas.width, canvas.height);
+// function animate() {
+//   requestAnimationFrame(animate);
+//   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  for (let i = 0; i < particlesArray.length; i++) {
-    particlesArray[i].update();
-  }
-}
+//   for (let i = 0; i < particlesArray.length; i++) {
+//     particlesArray[i].update();
+//   }
+// }
 
-init();
-animate();
+// init();
+// animate();
